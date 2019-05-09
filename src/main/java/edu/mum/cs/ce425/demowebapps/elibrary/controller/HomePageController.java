67cf2fe355dfee70ee4e9208b3d4040d4d8e3178
jules <juleswilfrid9@gmail.com>
2019-05-09 16:29:58 -0500

    First commit

---
+++ src/main/java/edu/mum/cs/ce425/demowebapps/elibrary/controller/HomePageController.java
@@ -0,0 +1,22 @@
+package edu.mum.cs.ce425.demowebapps.elibrary.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+	@GetMapping(value = { "/", "/elibrary", "/elibrary/home" })
+	public String displayHomePage() {
+	
+		return "home/index";
+
+	} 
+		@GetMapping(value = { "/elibrary/about" })
+		public String displayAboutPage() {
+		
+			return "home/about";
+		
+		
+	}
+
+}
---
+++ src/test/java/edu/mum/cs/ce425/demowebapps/elibrary/ElibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.mum.cs.ce425.demowebapps.elibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
